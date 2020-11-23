d12aa158f3e98150e6df5a326cbd5eb770f63fa4
ShiNoragami <Dona.boy12@gmail.com>
2020-11-23 13:55:42 +0100

    Implement firebase Google/fb auth

--- Go4Lunch/app/src/main/java/com/sn/go4lunch/MainActivity.java
+++ Go4Lunch/app/src/main/java/com/sn/go4lunch/MainActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.sn.go4lunch.base.BaseActivity;
+
+import java.util.Arrays;
+
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getFragmentLayout() { return R.layout.activity_main; }
+
+    private static final int RC_SIGN_IN = 123;
+
+    @OnClick(R.id.main_activity_button_login)
+    public void onClickLoginButton() {
+        this.startSignInActivity();
     }
+
+    //Google & Facebook auth
+     private void startSignInActivity(){
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(
+                    Arrays.asList(
+                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(), //GOOGLE
+                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build())) // FACEBOOK
+                .setIsSmartLockEnabled(false, true)
+                .setLogo(R.drawable.ic_logo_auth)
+                .build(),
+            RC_SIGN_IN);
+       }
+
+
 }
---
+++ Go4Lunch/app/src/main/java/com/sn/go4lunch/base/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.sn.go4lunch.base;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    //Life Cycle
+    @Override
+    protected void  onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        this.setContentView(this.getFragmentLayout());
+        ButterKnife.bind(this);
+    }
+
+    public abstract int getFragmentLayout();
+
+}
